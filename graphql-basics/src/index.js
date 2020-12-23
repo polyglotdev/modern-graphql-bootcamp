3d93986eacb5a60bbc5241ba39a5b530c9cc5fd6
Dom Hallan <dom@polyglotdev.com>
2020-12-22 18:50:58 -0600

    🎆 complete section 2-11 module

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,7 +1,36 @@
-import { add } from './math'
-import { message, name } from './myModule'
+import { GraphQLServer } from 'graphql-yoga'
 
-const sayHello = () => `Hello, ${name}!`
-console.log(sayHello())
-console.log(add(3, 10))
-console.log(message)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello() {
+      return `hello!`
+    },
+    name() {
+      return `Dom is my name!`
+    },
+    location() {
+      return `St. Louis, MO`
+    },
+    bio() {
+      return `My name is Dom and I live in STL.`
+    }
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+})
+
+server.start(() => {
+  console.log(`[server]: server is running at http://localhost:4000/`)
+})
--- graphql-basics/src/myModule.js
+++
@@ -1,4 +0,0 @@
-const message = 'Hello, World'
-const name = `Dom`
-
-export { message, name }
